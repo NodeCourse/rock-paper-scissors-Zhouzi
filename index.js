f50a4049ccebafc005a1ba7916e0a5e218924c55
Gabin <hello@gabinaureche.com>
2018-05-15 09:31:32 +0200

    Pick a random choice for the computer

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express');
+const randomItem = require('random-item');
 const choices = require('./choices.json');
 const app = express();
 
 app.set('view engine', 'pug');
 app.use(express.static('public'));
 
-app.use((req, res) => {
+app.get('/', (req, res) => {
     res.render('home', { choices });
+});
+
+app.get('/play/:choice', (req, res) => {
+    const userChoice = choices.find((choice) => {
+        return choice.id === req.params.choice;
+    });
+    const computerChoice = randomItem(choices);
+    res.render('play', { userChoice, computerChoice });
 });
 
 app.listen(3000, () => {
